718bb10b5b606162a4388af0ed9696a7f131e837
𝚆𝚘𝚠. 𝙲𝚘𝚖 <160624200+woww-com@users.noreply.github.com>
2024-07-04 11:43:06 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+const config = {
+  'noImage': 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhN74CcBgED5650EPwhm1ir78WYoOu10z_7Ww1lwW5u6ZNl2Ve9MK1DVsXE_O4svDpMGCDUrcYYYpi0sqzewLUtJzsysSEtqCru41FhWJeeqgxoAq-Hlh10PJYdbA35UczO4gQuuDBw0X6UULZkXZ3aTiu-n-R34tXtO4csVS6UtHTnlLSb6EB5ByvCIxfi/s800-rw/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.webp',
+  'regexChapter': /((chapter|episode|ep|ch|vol|volume|bab)(\.\s|\s|\.)(\d{1,4})(\.\d{1,2})?)/gi,
+  'regexScore': /(\d{1,2}\.\d{1,2})/,
+  'disqus_shortname': 'add_disqus_id_here',
+  'max_bookmark': 10,
+  'max_history': 10,
+  'liveSearch': true,
+  'readingBar': true
+};
